cace52d96a670292e35ffd92a8e56a7fbe90ded3
Jeffrey Neal <jeffneal11@gmail.com>
2022-12-08 11:54:21 -0600

    added smooth animation

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 function waves() {
   let container = document.getElementById("wave-container");
   let waveLength = 10;
-  //let htmlString = `<div class="waveSeg"></div>`;
-
-  //   let child = container;
-  //   child.innerHTML += htmlString;
 
   let divPrefix = `<div class="waveSeg">0`;
-  //   let htmlString;
+
   let divEnd = `</div>`;
   for (let i = 0; i < waveLength; i++) {
     divPrefix += `<div class="waveSeg">${i + 1}`;
     divEnd += `</div>`;
-    //container.innerHTML += htmlString;
-
-    // child = document.querySelector(`.${child.className} :nth-child(${i})`);
-    // child.innerHTML += htmlString;
-    // console.log(
-    //   document.querySelector(`.${container.className} :nth-child(${1})`)
-    // );
   }
+
   let htmlString = divPrefix + divEnd;
   container.innerHTML += htmlString;
 }
+
+var i = 0;
+
+function myLoop() {
+  setTimeout(function () {
+    // console.log(
+
+    // );
+    document.querySelectorAll(`.waveSeg`)[i].classList.add("animateWave");
+    i++;
+    if (i < 10) {
+      myLoop();
+    }
+  }, 100);
+}
+
+myLoop();
 
 waves();
